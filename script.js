5fa8470041856c964b9f56f537b9c158658603c6
x500x <109746464+x500x@users.noreply.github.com>
2024-07-27 22:58:26 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -21,13 +21,12 @@ xhr.addEventListener('readystatechange', function () {
   }
 });
 
-xhr.open('POST', 'https://www.123pan.com/b/api/file/upload_request');
-xhr.setRequestHeader('User-Agent', 'Reqable/2.20.2');
+xhr.open('POST', 'https://www.123pan.com/b/api/file/upload_request',true);
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjI0ODg3MTksImlhdCI6MTcyMTg4MzkxOSwiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.v5F4GY_nKvasmr9HbGPCgyX8mxhgvu9aNgSi6a04Ygg');
 xhr.setRequestHeader('Platform', 'open_platform');
 xhr.setRequestHeader('app-version', '3');
-xhr.setRequestHeader('Cookie', 'aliyungf_tc=1e32b8f370d1df293501ad648eb46c1f2420a4342346b260fc1338ecd40624c4');
 
 xhr.send(data);
+document.write('done');
 }
